cf1f7366689cae910afdc2f227e5d5fa2c3c92aa
eslam nawar <eslamnawar622@gmail.com>
2025-08-07 14:52:15 +0300

    Add solution for B - Anton and Digits

---
+++ B - Anton and Digits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int k2, k3, k5, k6;
+    cin >> k2 >> k3 >> k5 >> k6;
+
+    int cnt256 = min({k2, k5, k6});
+    k2 -= cnt256;
+    int cnt32 = min(k2, k3);
+
+    int result = cnt256 * 256 + cnt32 * 32;
+    cout << result << endl;
+
+    return 0;
+}
